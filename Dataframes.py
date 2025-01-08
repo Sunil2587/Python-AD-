4b36176d3ec280b102db7f277586e6656524e58c
Sunil2587 <161604906+Sunil2587@users.noreply.github.com>
2025-01-08 06:16:47 +0000

    price

--- Dataframes.py
+++ Dataframes.py
@@ -13,3 +13,56 @@ print(dict1)
 
 df = pd.DataFrame(dict1)
 print(df)
+
+
+#list comprehesion
+
+#list =[1,2,3,4]
+#res=[i+1 for i in range(1,20)]
+#print(res)
+
+list =[1,2,3,4,5,6,7,8,9]
+res=[i for i in list if i%2==0]
+print("even number",res)
+od=[i for i in list if i%2 !=0]
+print("odd number:",od)
+# lower and upper case letters in list
+words =["Lower","W","PYTHON"]
+lower =[i.lower() for i in words]
+print(lower)
+words =["ood","fss","sunil"]
+lower =[i.upper() for i in words]
+print(lower)
+
+# creating dictionary  comprehension from mtwo lists
+
+keys =["name","age","city"]
+values =["sunil",19,"Hyd"]
+list =[i for i in list]
+dict ={k:v for k,v in zip(keys,values)}
+print(dict)
+
+# tuple  comprehension 
+
+import math
+num = [1,4,16,9,25]
+sq=tuple(math.sqrt(i) for i in num)
+
+
+print(sq)
+
+
+# Give me the produt details with the price less then 500 use of dict comp
+
+products =[
+    {"name":"laptop","price":800},
+     {"name":"tablet","price":500},
+      {"name":"phone","price":400}
+]
+
+affordable_products = {i["name"]:i["price"] for i in products if i["price"]<500}
+print(affordable_products)
+
+
+
+
